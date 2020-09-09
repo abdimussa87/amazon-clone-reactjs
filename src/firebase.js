17809d2eee1cfb26b3cc4999f1423f38e745cc12
abdi mussa <abdimussa87@gmail.com>
2020-09-09 16:55:24 +0300

    Navbar/header completed

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCcK_6aoZ3TOLQWZwF8ieuKPS_A-vnLoyU",
+  authDomain: "clone-ea5e4.firebaseapp.com",
+  databaseURL: "https://clone-ea5e4.firebaseio.com",
+  projectId: "clone-ea5e4",
+  storageBucket: "clone-ea5e4.appspot.com",
+  messagingSenderId: "759120536414",
+  appId: "1:759120536414:web:fd30e851b6839fe37565cf",
+  measurementId: "G-6ECE1QWBD2"
+};
